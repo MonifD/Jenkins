487de0a1b5467c14734359ea961de98c5d51f154
Mohamad <douri_m@etna-alternance.net>
2024-05-06 00:28:21 +0200

    add project jenkins

---
+++ test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.amazon.fr")
+
+driver.find_element(By.ID , "sp-cc-rejectall-link").click()
+
+try:
+    search_box = driver.find_element(By.ID, "twotabsearchtextbox")
+    search_box.clear()
+    search_box.send_keys("carte de jeux")
+    search_box.send_keys(Keys.ENTER)
+    open_element = driver.find_element(By.XPATH, "//span[text()='BIERDORF Jeux de Cartes Poker - Étanches en Plastique Diamond Noir Nouveauté Jeu de Cartes 54, Playing Cards']").click()
+
+    price = driver.find_element(By.XPATH, "//*[@id=\"corePriceDisplay_desktop_feature_div\"]/div[1]/span[2]/span[2]")
+    print(price.text)
+except Exception as ex:
+    assert False
